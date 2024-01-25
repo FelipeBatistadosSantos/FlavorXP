105c136bf8bcb7b809926f62edf909b24652947b
zago <guilhermebzago10@gmail.com>
2024-01-25 15:16:19 -0300

    cadastre e login

--- angeline/forms.py
+++ angeline/forms.py
@@ -8,6 +8,19 @@ from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        # Remover mensagens de validação padrão
+        for field in self.fields.values():
+            field.widget.attrs.pop("title", None)
+            field.help_text = None
+
+        # Adicionar placeholders se desejar
+        self.fields['username'].widget.attrs['placeholder'] = 'Usuário'
+        self.fields['email'].widget.attrs['placeholder'] = 'Endereço de email'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Senha'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirmação de senha'
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
