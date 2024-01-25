75e2b67454b8b5e871006d14609ab00fdefeec01
Bia <biaalperes@gmail.com>
2024-01-25 14:26:44 -0300

    ok

---
+++ angeline/cidades.py
@@ -0,0 +1,14 @@
+cidades = [
+    {
+        1:{   
+        'nome': 'blumenau'
+        }
+    },
+
+    {
+        2:{
+        'nome':'brusque'
+
+        }
+    },
+]
--- angeline/models.py
+++ angeline/models.py
@@ -45,7 +45,4 @@ class CustomUser(AbstractBaseUser):
     
 
 class Produto(models.Model):
-    nome = models.CharField(max_length=100)
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
-    categoria = models.CharField(max_length=50)
     cidade = models.CharField(max_length=50)
