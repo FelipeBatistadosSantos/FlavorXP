a9066f825cf82e057d83a00e4540a11d16d8cebf
pcadmin <guilhermebzago10@gmail.com>
2024-01-30 16:48:16 -0300

    bd

--- angeline/models.py
+++ angeline/models.py
@@ -28,6 +28,25 @@ class CustomUser(AbstractBaseUser):
     def __str__(self):
         return self.email
     
+
+class Estado(models.Model):
+    codigo = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=50)
+    sigla = models.CharField(max_length=2)
+    
+    def __str__(self):
+        return self.nome
+
+
+class Cidade(models.Model):
+    codigo_uf = models.IntegerField()
+    codigo_ibge = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=50)
+    estado = models.ForeignKey(Estado, on_delete=models.CASCADE, default='')
+
+    def __str__(self):
+        return self.nome
+
     
 class CompleteCadastro(models.Model):
 
@@ -50,8 +69,8 @@ class CompleteCadastro(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE, default='')
     cep = models.CharField('cep', max_length=15, default='')
     cpf = CPFField('cpf')
-    cidade = models.CharField('cidade', max_length=50, default='')
-    estado = models.CharField('estado', max_length=2, default='')
+    cidade = models.ForeignKey(Cidade, on_delete=models.SET_NULL, null=True, blank=True)
+    estado = models.ForeignKey(Estado, on_delete=models.SET_NULL, null=True, blank=True)
     telefone = models.CharField('telefone', max_length=11, default='')
     nascimento = models.CharField('nascimento', max_length=10)
     sobre = models.TextField('sobre', default='')
@@ -61,27 +80,6 @@ class CompleteCadastro(models.Model):
     comidaf = models.CharField('comida', max_length=50)
     bebida = models.CharField('bebida',max_length=50)
     restricao = models.CharField('restricao', choices=RESTRICAO_CHOICES, max_length=30)
-    
-
-class Estado(models.Model):
-    codigo = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=50)
-    sigla = models.CharField(max_length=2)
-
-    def __str__(self):
-        return self.nome
-
-class Cidade(models.Model):
-    codigo = models.IntegerField(primary_key=True)  
-    nome = models.CharField(max_length=50)
-    estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
-    latitude = models.FloatField(default=0.0)
-    longitude = models.FloatField(default=0.0)
-    capital = models.BooleanField(default=False)
-    codigo_uf = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.nome
 
 
 class Host(models.Model):
