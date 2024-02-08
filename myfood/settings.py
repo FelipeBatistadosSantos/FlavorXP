82a37de6ae242bd0c863cfd834d1fff8b99b0ccc
Arthur <victorgabrieljubileu@gmail.com>
2024-02-08 16:00:20 -0300

    obrigado filipe Deus abenções

--- myfood/settings.py
+++ myfood/settings.py
@@ -147,12 +147,10 @@ DECIMAL_SEPARATOR = ','
 
 # settings.py
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'your_smtp_server'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
+EMAIL_HOST_USER = 'flavorxp@gmail.com'
+EMAIL_HOST_PASSWORD = 'dvdp dpro kbqi ckgb'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your_email@example.com'
-EMAIL_HOST_PASSWORD = 'your_email_password'
-
-
-
